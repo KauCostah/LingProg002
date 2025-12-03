f544cc3968accf4307e21f67288f3ceb81f25800
Kauani Costa <jesuskau341@gmail.com>
2025-12-03 14:05:57 -0300

    Update Main.java

--- LPOO02/src/atividade/Main.java
+++ LPOO02/src/atividade/Main.java
@@ -1,9 +1,9 @@
 package atividade;
 
-//Classe principal para testar o sistema
+
 public class Main {
  public static void main(String[] args) {
-     // Criando uma pessoa física
+     
      PessoaFisica pessoaFisica = new PessoaFisica(
          "Luis", 
          35, 
@@ -11,7 +11,6 @@ public class Main {
          "067.2598-29"
      );
      
-     // Criando uma pessoa jurídica
      PessoaJuridica pessoaJuridica = new PessoaJuridica(
          "Empresa ABC Ltda", 
          10, 
@@ -19,12 +18,12 @@ public class Main {
          "12.345.678/0001-90"
      );
      
-     // Exibindo os dados
+    
      pessoaFisica.showData();
      System.out.println();
      pessoaJuridica.showData();
      
-     // Demonstrando o uso dos métodos getters e setters
+   
      System.out.println("\n=== ALTERANDO DADOS ===");
      pessoaFisica.setEndereco("Nova Rua, 456 - Feira de Santana - BA");
      pessoaFisica.setIdade(36);
@@ -32,7 +31,7 @@ public class Main {
      System.out.println("Dados atualizados da pessoa física:");
      pessoaFisica.showData();
      
-     // Exemplo de polimorfismo
+    
      System.out.println("\n=== EXEMPLO DE POLIMORFISMO ===");
      Pessoa[] pessoas = {pessoaFisica, pessoaJuridica};
      
@@ -42,4 +41,5 @@ public class Main {
          System.out.println();
      }
  }
+
 }
